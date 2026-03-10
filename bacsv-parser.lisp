d9fc99149d238ceadd10a7831ae0656627bb9a67
melt <newsletter.encode113@passinbox.com>
2026-03-10 18:04:34 +0100

    CSV-PARSER: name change, added comments... WIP

--- bacsv-parser.lisp
+++ bacsv-parser.lisp
@@ -1,3 +1,5 @@
+; bacsv-parser.lisp
+
 #!/usr/bin/sbcl --script
 
 ;(defun print-lines (f)
@@ -6,6 +8,7 @@
 ;     (format t "~a~%" line)
 ;     (print-lines f))))
 
+; field production
 (defun prod-field (word)
   (let ((code (char-code (char word 0)))
         (rst (subseq word 1)))
@@ -14,30 +17,45 @@
              (not (= code 44))) ; Comma.
         (prod-field rst)
         t)))
+; end field production
 
+; record production
 (defun prod-record (word)
   )
+; end record production
 
+; parse record
 (defun parse-record (str)
   (format t "~a~%" str))
+; end parse record
 
+; parse records 
 (defun parse-records (file)
   (let ((line (read-line file nil)))
     (unless (null line)
       (format t "~a~%" (prod-field line))
       (parse-records file))))
+; end parse records 
 
+; parse csv
 (defun parse-csv (fname)
   (with-open-file (f fname)
     (parse-records f)))
+; end parse csv
 
+; usage
 (defun usage ()
   (format t "Usage: parse-csv.lisp FILE~%"))
+; end usage
 
+; main
 (defun main (args)
   (if (= (length args) 2)
       (parse-csv (cadr args))
       (usage)))
+; end main
 
 ;; Only works with SBCL.
 (main *posix-argv*)
+
+; bacsv-parser.lisp
